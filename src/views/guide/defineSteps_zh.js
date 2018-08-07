26ff2bafdd25a528e678c3689c85b29ca942c348
295277302@qq.com <295277302@qq.com>
2018-08-07 21:44:19 +0800

    dev:init

---
+++ src/views/guide/defineSteps_zh.js
@@ -0,0 +1,52 @@
+const steps_zh = [
+  {
+    element: '.hamburger-container',
+    popover: {
+      title: '导航页',
+      description: '打开 && 缩起 导航',
+      position: 'bottom'
+    }
+  },
+  {
+    element: '.breadcrumb-container',
+    popover: {
+      title: '当前页面',
+      description: '指示当前页面位置',
+      position: 'bottom'
+    }
+  },
+  {
+    element: '.screenfull',
+    popover: {
+      title: '全屏',
+      description: '将页面带入全屏',
+      position: 'left'
+    }
+  },
+  {
+    element: '.international-icon',
+    popover: {
+      title: '语言',
+      description: '切换系统语言',
+      position: 'left'
+    }
+  },
+  {
+    element: '.theme-switch',
+    popover: {
+      title: '主题',
+      description: '自定义系统主题',
+      position: 'left'
+    }
+  },
+  {
+    element: '.tags-view-container',
+    popover: {
+      title: '标签页',
+      description: '您访问过的页面的历史',
+      position: 'bottom'
+    }
+  }
+]
+
+export default steps_zh
---
+++ src/views/guide/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app-container">
+    <p class="warn-content">
+      {{$t('guide.description')}}
+      <a href="https://github.com/kamranahmedse/driver.js" target="_blank">driver.js.
+      </a>
+    </p>
+    <el-button icon='el-icon-question' type="primary" @click.prevent.stop="guide">{{$t('guide.button')}}</el-button>
+  </div>
+</template>
+
+<script>
+import * as Driver from 'driver.js' // import driver.js
+import 'driver.js/dist/driver.min.css' // import driver.js css
+import steps from './defineSteps'
+import steps_zh from './defineSteps_zh'
+
+export default {
+  name: 'guide',
+  data() {
+    return {
+      driver: null
+    }
+  },
+  mounted() {
+    this.driver = new Driver()
+  },
+  methods: {
+    guide() {
+      if (this.$store.getters.language === 'en') {
+        this.driver.defineSteps(steps)
+      } else {
+        this.driver.defineSteps(steps_zh)
+      }
+      this.driver.start()
+    }
+  }
+}
+</script>
